d348fca8743f89e3411332e24f3ab2d690dcfb88
SvenTigges <sven.tigges@web.de>
2021-05-26 14:27:48 +0200

    Objekte 2 Hierarchie

--- script_01.js
+++ script_01.js
@@ -25,17 +25,50 @@ let person = {
 };
 
 
-ausgabe(person);
-ausgabe(person.firstName);
-ausgabe(person.familiyName);
-ausgabe(person.salary[0]);
-ausgabe(person.hasAccess);
-ausgabe(person.sayHello()); 
+// ausgabe(person);
+// ausgabe(person.firstName);
+// ausgabe(person.familiyName);
+// ausgabe(person.salary[0]);
+// ausgabe(person.hasAccess);
+// ausgabe(person.sayHello()); 
 let txt = "ich bin " + person.firstName + " " + 
-                      person.familiyName + " ud verdiene " + 
-                      person.salary[0] + " p.a.";
-ausgabe(txt);
+                     person.familiyName + " ud verdiene " + 
+                     person.salary[0] + " p.a.";
+// ausgabe(txt);
+/** Ausgabe */
+
+
+/**** Objekte 2 Hierarchie ****/
+
+// Teifer, tiefer -Irgendwo in der Tiefe gibt es en Licht 
+// der Baikalsee ist mit 1642 m der tiefste See der Erde
+// deep - deeper - deeperst
+// Nested Object
+
+let baikal = {
+                wert: "10m",
+                deep:{
+                        deeper:{
+                                deepest: "Das licht - auf 1642 m"
+                               }
+                     } 
+
+
+
+
+};
+
+ausgabe(baikal);
+ausgabe(baikal.wert);
+ausgabe(baikal.deep.deeper.deepest);
+
+
+
+
+
+
+
 /** Ausgabe */
 function ausgabe(outputStr) {
-        console.log(outputStr);
-    }
+    console.log(outputStr);
+}
